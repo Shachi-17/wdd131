9367add9e9e4d5f219fa4f3b840271ffe6ec7a4b
Shachi-17 <145852607+Shachi-17@users.noreply.github.com>
2024-12-13 19:29:58 +0800

    FINAL COMMIT

--- scripts/site.js
+++ scripts/site.js
@@ -1,5 +1,20 @@
+const yearElement = document.getElementById("currentyear");
+if (yearElement) {
+    yearElement.textContent = new Date().getFullYear();
+}
 
-document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+const modifiedElement = document.getElementById("lastModified");
+if (modifiedElement) {
+    modifiedElement.textContent = `Last Modification: ${document.lastModified}`;
+}
 
 
-document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
